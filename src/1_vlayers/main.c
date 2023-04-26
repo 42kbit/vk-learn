cea36164e5d489b49bacae97f0da9366df23795e
Serhii Ostrikov <serostrikov@gmail.com>
2023-04-26 21:12:56 +0300

    better code organize

--- src/1_vlayers/main.c
+++ src/1_vlayers/main.c
@@ -24,15 +24,27 @@ const char* vkapp_required_vlayers[] = {
 /* All info needed for rendeing Vulkan Application. */
 struct vkapp {
 	GLFWwindow*		 glfw_window;
-	GArray*			 vlayers;
 	GArray*			 exts;
 	VkInstance 		 instance;
 	VkPhysicalDevice	 physdev;
+#ifdef __VK_VLAYERS_NEEDED
+	GArray*			 vlayers;
+#endif
 #ifdef DEBUG
 	VkDebugUtilsMessengerEXT debug_messenger;
 #endif
 };
 struct vkapp __vkapp; /* Shall not be accessed directly */
+
+struct __cmp_VkLayerProperties_name {
+	VkLayerProperties** dst;
+	const char* search_for;
+};
+
+static inline bool
+g_array_cmp_VkLayerProperties_name (GArray*  arr,
+		     		    guint    idx,
+		     		    gpointer _udata);
 
 #ifdef __VK_VLAYERS_NEEDED
 static inline bool
@@ -40,15 +52,29 @@ vkapp_matches_vlayers (struct vkapp* p,
 		       const char**  vlneeded,
 		       guint	     vlneeded_len,
 		       const char**  vlfailed_on);
-#endif
 
-#ifdef DEBUG
-static VKAPI_ATTR VkBool32 VKAPI_CALL
-vk_debug_callback (VkDebugUtilsMessageSeverityFlagBitsEXT      msg_severity,
-		VkDebugUtilsMessageTypeFlagsEXT	       	    message_type,
-		const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
-		void* _udata);
-#endif
+static inline bool
+vkapp_matches_vlayers (struct vkapp* p,
+		       const char**  vlneeded,
+		       guint	     vlneeded_len,
+		       const char**  vlfailed_on)
+{
+	VkLayerProperties* found;
+	struct __cmp_VkLayerProperties_name args;
+
+	guint i = 0;
+	for (i = 0; i < vlneeded_len; i++) {
+		found = NULL;
+		args.dst = &found;
+		args.search_for = vlneeded[i];
+		g_array_traverse (p->vlayers, g_array_cmp_VkLayerProperties_name, &args);
+		if (!found) {
+			*vlfailed_on = args.search_for;
+			return FALSE;
+		}
+	}
+	return TRUE;
+} 
 
 static inline GArray*
 vk_get_vlayers (void)
@@ -67,30 +93,38 @@ vk_get_vlayers (void)
 	g_array_set_size (vlayers, vlayers_cnt);
 	return vlayers;
 }
+#endif
 
-static inline GArray*
-vk_get_required_ext (void)
+#ifdef DEBUG
+static VKAPI_ATTR VkBool32 VKAPI_CALL
+vk_debug_callback (VkDebugUtilsMessageSeverityFlagBitsEXT      msg_severity,
+		VkDebugUtilsMessageTypeFlagsEXT	       	    message_type,
+		const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+		void* _udata);
+
+
+static inline const char *
+getstr_VkDebugUtilsMessageSeverityFlagBitsEXT (VkDebugUtilsMessageSeverityFlagBitsEXT msg_severity)
 {
-	GArray* p;
-	const char** glfw_exts;
-	const char * const debug_ext = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
-	guint glfw_n;
-	guint nexts;
+	switch (msg_severity) {
+		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT: return "VRB";
+		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:    return "LOG";
+		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT: return "WRN";
+		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:   return "ERR";
+		default: return "INV"; /* Invalid */
+	}
+}
 
-	glfw_exts = glfwGetRequiredInstanceExtensions (&glfw_n);
-	nexts = glfw_n;
-
-#ifdef DEBUG
-	nexts += 1; /* VK_EXT_DEBUG_UTILS_EXTENSION_NAME */
-#endif
-
-	p = g_array_sized_new (FALSE, TRUE, sizeof (const char*), nexts);
-	g_array_append_vals (p, glfw_exts, glfw_n);
-
-#ifdef DEBUG
-	g_array_append_val  (p, debug_ext);
-#endif
-	return p;
+static VKAPI_ATTR VkBool32 VKAPI_CALL
+vk_debug_callback (VkDebugUtilsMessageSeverityFlagBitsEXT      msg_severity,
+		   VkDebugUtilsMessageTypeFlagsEXT	       msg_type,
+		   const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+		   void* _udata)
+{
+	const char* severity_str = getstr_VkDebugUtilsMessageSeverityFlagBitsEXT (msg_severity);
+	
+	g_printerr ("[%s] %s\n", severity_str, callback_data->pMessage);
+	return msg_severity < VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT;
 }
 
 static inline VkResult
@@ -119,6 +153,33 @@ term_VkDebugUtilsMessengerEXT (VkDebugUtilsMessengerEXT     p,
 	VCOPY (func, PFN_vkDestroyDebugUtilsMessengerEXT, _func);
 	func (instance, p, callbacks);
 }
+#endif
+
+static inline GArray*
+vk_get_required_ext (void)
+{
+	GArray* p;
+	const char** glfw_exts;
+	const char * const debug_ext = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+	guint glfw_n;
+	guint nexts;
+
+	glfw_exts = glfwGetRequiredInstanceExtensions (&glfw_n);
+	nexts = glfw_n;
+
+#ifdef DEBUG
+	nexts += 1; /* VK_EXT_DEBUG_UTILS_EXTENSION_NAME */
+#endif
+
+	p = g_array_sized_new (FALSE, TRUE, sizeof (const char*), nexts);
+	g_array_append_vals (p, glfw_exts, glfw_n);
+
+#ifdef DEBUG
+	g_array_append_val  (p, debug_ext);
+#endif
+	return p;
+}
+
 
 static inline void
 init_vkapp_instance (struct vkapp* p)
@@ -195,7 +256,9 @@ init_vkapp (struct vkapp* p,
 	    GLFWwindow*	  glfw_window)
 {
 	p->exts = vk_get_required_ext ();
+#ifdef __VK_VLAYERS_NEEDED
 	p->vlayers = vk_get_vlayers ();
+#endif
 	p->glfw_window = glfw_window;
 	init_vkapp_instance (p);
 }
@@ -206,15 +269,13 @@ term_vkapp (struct vkapp* p)
 #ifdef DEBUG
 	term_VkDebugUtilsMessengerEXT (p->debug_messenger, p->instance, NULL);
 #endif
+#ifdef __VK_VLAYERS_NEEDED
 	g_array_free (p->vlayers, TRUE);
+#endif
 	vkDestroyInstance (p->instance, NULL);
 	glfwDestroyWindow (p->glfw_window);
 }
 
-struct __cmp_VkLayerProperties_name {
-	VkLayerProperties** dst;
-	const char* search_for;
-};
 
 static inline bool
 g_array_cmp_VkLayerProperties_name (GArray*  arr,
@@ -230,57 +291,6 @@ g_array_cmp_VkLayerProperties_name (GArray*  arr,
 		return FALSE;
 	}
 	return TRUE;
-}
-
-static inline bool
-vkapp_matches_vlayers (struct vkapp* p,
-		       const char**  vlneeded,
-		       guint	     vlneeded_len,
-		       const char**  vlfailed_on)
-{
-#ifdef __VK_VLAYERS_NEEDED
-	VkLayerProperties* found;
-	struct __cmp_VkLayerProperties_name args;
-
-	guint i = 0;
-	for (i = 0; i < vlneeded_len; i++) {
-		found = NULL;
-		args.dst = &found;
-		args.search_for = vlneeded[i];
-		g_array_traverse (p->vlayers, g_array_cmp_VkLayerProperties_name, &args);
-		if (!found) {
-			*vlfailed_on = args.search_for;
-			return FALSE;
-		}
-	}
-#endif
-	return TRUE;
-} 
-
-static inline const char *
-getstr_VkDebugUtilsMessageSeverityFlagBitsEXT (VkDebugUtilsMessageSeverityFlagBitsEXT msg_severity)
-{
-	switch (msg_severity) {
-		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT: return "LOG";
-		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:    return "LOG";
-		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT: return "WRN";
-		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:   return "ERR";
-		default: return "INV"; /* Invalid */
-	}
-}
-
-static VKAPI_ATTR VkBool32 VKAPI_CALL
-vk_debug_callback (VkDebugUtilsMessageSeverityFlagBitsEXT      msg_severity,
-		   VkDebugUtilsMessageTypeFlagsEXT	       msg_type,
-		   const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
-		   void* _udata)
-{
-#ifdef DEBUG
-	const char* severity_str = getstr_VkDebugUtilsMessageSeverityFlagBitsEXT (msg_severity);
-	
-	g_printerr ("[%s] %s\n", severity_str, callback_data->pMessage);
-	return msg_severity < VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT;
-#endif
 }
 
 int
